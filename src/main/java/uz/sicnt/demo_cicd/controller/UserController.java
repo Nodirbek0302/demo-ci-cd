a0df2c016d30311d9369ab0d78f7f8c5e58067d1
n.norov <no0404ir@gmail.com>
2025-10-02 16:27:25 +0500

    init commit

---
+++ src/main/java/uz/sicnt/demo_cicd/controller/UserController.java
@@ -0,0 +1,17 @@
+package uz.sicnt.demo_cicd.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    @GetMapping("/get")
+    public String getUsers() {
+        return "Users";
+    }
+}
