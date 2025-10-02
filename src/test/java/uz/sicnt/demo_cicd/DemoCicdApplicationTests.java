2960d239808fd58c51c581d31694f745ea85efb5
n.norov <no0404ir@gmail.com>
2025-10-02 16:27:25 +0500

    init commit

---
+++ src/test/java/uz/sicnt/demo_cicd/DemoCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.sicnt.demo_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
