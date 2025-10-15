1fce996b06800738813bfe5e0ad30cdf535b9da4
Nodirbek <no0404ir@gmail.com>
2025-10-15 17:12:46 +0500

    birinchi commit

--- src/main/java/uz/sicnt/demo_cicd/controller/UserController.java
+++ src/main/java/uz/sicnt/demo_cicd/controller/UserController.java
@@ -12,6 +12,6 @@ public class UserController {
 
     @GetMapping("/get")
     public String getUsers() {
-        return "User karochi chiqdi CI - CD ishladi akarochi";
+        return "User karochi chiqdi Alisher bro ";
     }
 }
