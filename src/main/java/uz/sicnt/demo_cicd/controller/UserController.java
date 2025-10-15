bb0833df6a89445343c2056ea68fb1433a591b34
Nodirbek <no0404ir@gmail.com>
2025-10-15 11:00:45 +0500

    cicd test

--- src/main/java/uz/sicnt/demo_cicd/controller/UserController.java
+++ src/main/java/uz/sicnt/demo_cicd/controller/UserController.java
@@ -12,6 +12,6 @@ public class UserController {
 
     @GetMapping("/get")
     public String getUsers() {
-        return "User karochi chiqdi";
+        return "User karochi chiqdi CI - CD ishladi akarochi";
     }
 }
